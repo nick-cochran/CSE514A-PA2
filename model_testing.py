8d2764d2b4a20dfd98b577484bc4927218f732e5
nick-cochran <c.nick@wustl.edu>
2024-11-20 14:10:37 -0600

    added more things, got more done

--- model_testing.py
+++ model_testing.py
@@ -11,6 +11,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import time
+
 
 # set reduce bool
 if len(sys.argv) > 1 and sys.argv[1] == 'reduce':
@@ -30,40 +32,51 @@ A_train, A_test = train_test_split(population_A, test_size=0.1, random_state=42)
 B_train, B_test = train_test_split(population_B, test_size=0.1, random_state=42)
 C_train, C_test = train_test_split(population_C, test_size=0.1, random_state=42)
 
-A_train.to_csv('population_A_train.csv')
-A_test.to_csv('population_A_test.csv')
-B_train.to_csv('population_B_train.csv')
-B_test.to_csv('population_B_test.csv')
-C_train.to_csv('population_C_train.csv')
-C_test.to_csv('population_C_test.csv')
+# A_train.to_csv('population_A_train.csv')
+# A_test.to_csv('population_A_test.csv')
+# B_train.to_csv('population_B_train.csv')
+# B_test.to_csv('population_B_test.csv')
+# C_train.to_csv('population_C_train.csv')
+# C_test.to_csv('population_C_test.csv')
 
 # Split the data into features and target
 feature_labels = ['ID', 'Reason for absence', 'Day of the week', 'Seasons', 'Transportation expense',
                   'Distance from Residence to Work', 'Service time', 'Age', 'Work load Average/day ',
                   'Hit target', 'Disciplinary failure', 'Education', 'Son', 'Social drinker',
                   'Social smoker', 'Pet', 'Weight', 'Height', 'Body mass index', 'Month Groups']
-reduced_feature_labels = ['ID', 'Reason for absence', 'Day of the week', 'Seasons',
-                  'Transportation expense', 'Distance from Residence to Work', 'Service time',
-                  'Age', 'Work load Average/day ', 'Hit target', 'Disciplinary failure', 'Education',
-                  'Son', 'Social drinker', 'Social smoker', 'Pet', 'Weight', 'Height', 'Body mass index',
-                  'Month Groups']
+reduced_feature_labels = ['Reason for absence', 'Day of the week', 'Seasons', 'Work load Average/day ',
+                          'Disciplinary failure', 'Son', 'Social drinker', 'Pet', 'Body mass index', 'Month Groups']
+
+target = populations['Absenteeism time in hours']
+A_target = population_A['Absenteeism time in hours']
+B_target = population_B['Absenteeism time in hours']
+C_target = population_C['Absenteeism time in hours']
 
 if reduce:
     features = populations[reduced_feature_labels]
     A_features = population_A[reduced_feature_labels]
     B_features = population_B[reduced_feature_labels]
     C_features = population_C[reduced_feature_labels]
+
+    # population_A = population_A[reduced_feature_labels + ['Absenteeism time in hours']]
+    # population_B = population_B[reduced_feature_labels + ['Absenteeism time in hours']]
+    # population_C = population_C[reduced_feature_labels + ['Absenteeism time in hours']]
+    #
+    # A_train, A_test = train_test_split(population_A, test_size=0.1, random_state=42)
+    # B_train, B_test = train_test_split(population_B, test_size=0.1, random_state=42)
+    # C_train, C_test = train_test_split(population_C, test_size=0.1, random_state=42)
+    #
+    # A_train.to_csv('population_A_train_reduced.csv')
+    # A_test.to_csv('population_A_test_reduced.csv')
+    # B_train.to_csv('population_B_train_reduced.csv')
+    # B_test.to_csv('population_B_test_reduced.csv')
+    # C_train.to_csv('population_C_train_reduced.csv')
+    # C_test.to_csv('population_C_test_reduced.csv')
 else:
     features = populations[feature_labels]
     A_features = population_A[feature_labels]
     B_features = population_B[feature_labels]
     C_features = population_C[feature_labels]
-
-
-target = populations['Absenteeism time in hours']
-A_target = population_A['Absenteeism time in hours']
-B_target = population_B['Absenteeism time in hours']
-C_target = population_C['Absenteeism time in hours']
 
 # Split the data into training and testing sets
 X_training, X_testing, y_training, y_testing = train_test_split(features, target, test_size=0.1, random_state=42)
@@ -71,21 +84,7 @@ AX_train, AX_test, Ay_train, Ay_test = train_test_split(A_features, A_target, te
 BX_train, BX_test, By_train, By_test = train_test_split(B_features, B_target, test_size=0.1, random_state=42)
 CX_train, CX_test, Cy_train, Cy_test = train_test_split(C_features, C_target, test_size=0.1, random_state=42)
 
-# Create the model
-SVM_lin = svm.SVC(kernel='linear')  # SVM hyperparameter value is the kernel type
-SVM_poly = svm.SVC(kernel='poly')
-SVM_rbf = svm.SVC(kernel='rbf')
-# model.fit(X_train, y_train)
-# print(model.score(X_test, y_test))
-
-score_lin = cross_val_score(SVM_lin, X_training, y_training, cv=5)
-score_poly = cross_val_score(SVM_poly, X_training, y_training, cv=5)
-score_rbf = cross_val_score(SVM_rbf, X_training, y_training, cv=5)
-print("SVM linear cross validation score: ", score_lin)
-print("SVM polynomial cross validation score: ", score_poly)
-print("SVM RBF cross validation score: ", score_rbf)
-
-def svm_cross_validation(X_training, y_training):
+def svm_cross_validation(X_training, y_training, population):
     SVMs = [svm.SVC(kernel='linear'), svm.SVC(kernel='poly'), svm.SVC(kernel='rbf')]
     accuracy_results = []
 
@@ -101,24 +100,30 @@ def svm_cross_validation(X_training, y_training):
     print(f"Best kernel: {best_kernel}")
 
     # train best model on test set
+    start_training = time.time()
     final_model = svm.SVC(kernel=best_kernel)
     final_model.fit(X_training, y_training)
+    end_training = time.time()
+    print(f"\033[1mTraining time: {end_training - start_training} for population {population}\033[0m")
+    start_prediction = time.time()
     y_test_pred = final_model.predict(X_testing)
+    end_prediction = time.time()
+    print(f"\033[1mPrediction time: {end_prediction - start_prediction} for population {population}\033[0m")
 
     # performance of test set
     test_accuracy = accuracy_score(y_testing, y_test_pred)
     print(f"Test Accuracy with k={best_kernel}: {test_accuracy:.4f}")
 
-    # plot scores per kernel
-    # plt.figure(figsize=(10, 6))
-    # plt.plot(kNN_values, accuracy_results, marker='o', label='Accuracy')
-    # plt.xlabel('Number of Neighbors (k)')
-    # plt.ylabel('Cross-Validation Accuracy')
-    # plt.title('5-Fold Cross-Validation Results for SVM')
-    # plt.legend()
-    # plt.show()
+    kernels = ['linear', 'poly', 'rbf']
+    colors = ['skyblue', 'lightgreen', 'salmon']
+    plt.figure(figsize=(10, 6))
+    plt.bar(kernels, accuracy_results, color=colors)
+    plt.xlabel('Kernels')
+    plt.ylabel('Cross-Validation Accuracy')
+    plt.title('5-Fold Cross-Validation Results for SVM Kernels on population ' + population)
+    plt.show()
 
-def knn_cross_validation(X_training, y_training):
+def knn_cross_validation(X_training, y_training, population):
     kNN_values = [3, 7, 11, 13, 17]
     accuracy_results = []
 
@@ -136,9 +141,15 @@ def knn_cross_validation(X_training, y_training):
     print(f"Best k: {best_k}")
 
     # train best model on test set
+    start_training = time.time()
     final_model = KNeighborsClassifier(n_neighbors=best_k)
     final_model.fit(X_training, y_training)
+    end_training = time.time()
+    print(f"\033[1mTraining time: {end_training - start_training} for population {population}\033[0m")
+    start_prediction = time.time()
     y_test_pred = final_model.predict(X_testing)
+    end_prediction = time.time()
+    print(f"\033[1mPrediction time: {end_prediction - start_prediction} for population {population}\033[0m")
 
     # performance of test set
     test_accuracy = accuracy_score(y_testing, y_test_pred)
@@ -150,7 +161,7 @@ def knn_cross_validation(X_training, y_training):
     # plt.plot(kNN_values, f1_results, marker='s', label='F1 Score')
     plt.xlabel('Number of Neighbors (k)')
     plt.ylabel('Cross-Validation Accuracy')
-    plt.title('5-Fold Cross-Validation Results for kNN')
+    plt.title('5-Fold Cross-Validation Results for kNN on population ' + population)
     plt.legend()
     plt.show()
 
@@ -160,30 +171,30 @@ def knn_cross_validation(X_training, y_training):
 
 
 
-print("\033[1mKNN cross validation on Full Data\033[0m")
-knn_cross_validation(X_training, y_training)
+# print("\033[1mKNN cross validation on Full Data\033[0m")
+# knn_cross_validation(X_training, y_training)
 
 print("\033[1mKNN cross validation on Population A\033[0m")
-knn_cross_validation(AX_train, Ay_train)
+knn_cross_validation(AX_train, Ay_train, 'A')
 
 print("\033[1mKNN cross validation on Population B\033[0m")
-knn_cross_validation(BX_train, By_train)
+knn_cross_validation(BX_train, By_train, 'B')
 
 print("\033[1mKNN cross validation on Population C\033[0m")
-knn_cross_validation(CX_train, Cy_train)
+knn_cross_validation(CX_train, Cy_train, 'C')
 
 
-print("\033[1mSVM cross validation on Full Data\033[0m")
-svm_cross_validation(X_training, y_training)
+# print("\033[1mSVM cross validation on Full Data\033[0m")
+# svm_cross_validation(X_training, y_training)
 
 print("\033[1mSVM cross validation on Population A\033[0m")
-svm_cross_validation(AX_train, Ay_train)
+svm_cross_validation(AX_train, Ay_train, 'A')
 
 print("\033[1mSVM cross validation on Population B\033[0m")
-svm_cross_validation(BX_train, By_train)
+svm_cross_validation(BX_train, By_train, 'B')
 
 print("\033[1mSVM cross validation on Population C\033[0m")
-svm_cross_validation(CX_train, Cy_train)
+svm_cross_validation(CX_train, Cy_train, 'C')
 
 
 
